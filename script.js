90edd58231273dd912294e7abe94cb0432231045
UTCJ-Chavez <al25220431@utcj.edu.mx>
2026-04-24 18:56:56 -0600

    wip

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.btn');
+const products = document.querySelectorAll('.product');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelector('.active').classList.remove('active');
+        button.classList.add('active');
+        const filter = button.getAttribute('data-filter');
+        products.forEach(p => {
+            p.style.display = (filter === 'all' || p.getAttribute('data-category') === filter) ? 'block' : 'none';
+        });
+    });
+});
